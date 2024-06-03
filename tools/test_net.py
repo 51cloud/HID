283bdc5b00150e1e79fae9ca6da4174c00ebe56c
Zhuo Zhou <32673453+51cloud@users.noreply.github.com>
2024-06-03 11:27:13 +0800

    Update test_net.py

--- tools/test_net.py
+++ tools/test_net.py
@@ -54,34 +54,13 @@ def perform_test(test_loader, model, test_meter, cfg, writer=None):
     max_indices_array = []
     for cur_iter, (person_infos, videos, labels, video_idx, meta) in enumerate(test_loader):
         if cfg.NUM_GPUS:
-            # Transfer the data to the current GPU device.
-            # if isinstance(inputs, (list,)):
-            #     for i in range(len(inputs)):
-            #         inputs[i] = inputs[i].cuda(non_blocking=True)
-            # else:
-            #     inputs = inputs.cuda(non_blocking=True)
-
-            # Transfer the data to the current GPU device.
+            
             labels = labels.cuda()
-            # video_idx = video_idx.cuda()
-            # for key, val in meta.items():
-            #     if isinstance(val, (list,)):
-            #         for i in range(len(val)):
-            #             val[i] = val[i].cuda(non_blocking=True)
-            #     else:
-            #         meta[key] = val.cuda(non_blocking=True)
+            
 
             # Perform the forward pass.
-            # inputs1 = [inputs[0],]
             videos = videos[0]
-            tymode = 'test'
-            print('videos:', videos)
-            # preds, ps_scores, ns_scores = model(inputs1, inputs2, tymode)
-            # print("inputs1:", inputs1) # [tensor([[[[[
-            # print("frames_clips:", frames_clips) # [[tensor([[[[[
-            # print("label_clips:", label_clips) # [(
-            # print("frames_clips.shape(), label_clips.shape():", len(frames_clips), frames_clips[0][0].shape, len(label_clips))
-            # frames_clips.shape(), label_clips.shape(): 181 torch.Size([32, 3, 8, 224, 224]) 181
+            tymode = 'test'           
             outputs, _ = model(person_infos, videos, tymode)
             before = False
             theft = False
@@ -109,45 +88,7 @@ def perform_test(test_loader, model, test_meter, cfg, writer=None):
                 preds = before_pred
             if theft:
                 preds = theft_pred
-
-            # frames_clips_lenth = len(frames_clips)
-            # similars = []
-            # for i in range(frames_clips_lenth):
-            #     # print('i=', i)
-            #     input_clip = [frames_clips[0][0],]
-            #     preds_clip, feature = model(input_clip, videos, tymode)
-            #     # feature_equ = torch.eq(feature_ori[0], feature[0], out=None)
-            #     # print("feature_equ:", feature_equ)
-            #     similarity = similar_score(feature_ori[0], feature[0], weight=1/100)
-            #     # 初始化模型
-            #     # model1 = AttentionSimilarity(input_size=2048, hidden_size=1)
-            #     # model1 = model1.to('cuda:0')
-            #     # # 计算两个特征之间的相似度
-            #     #
-            #     # similarity = model1(feature_ori[0], feature[0], preds_clip)
-            #     print('similarity=', i, similarity)
-            #     similars.append(similarity)
-            # print('similars:', similars)
-
-            # for i in range(len(feature)):
-            #     sss= []
-            #     ss = feature[i][0][0][0].cpu().detach().numpy()
-            #     for j in range(len(ss)):
-            #         sss.append(ss[j])
-            #     # print('ss', sss)
-            #     label = labels[i].cpu().detach().numpy()
-            #     # print('label:', label)
-            #
-            #     with open('/public/home/zhouz/perl5/visual/text/baseline.txt', 'a') as file:
-            #         file.write(str(sss) + '\n')
-            #     with open('/public/home/zhouz/perl5/visual/text/baseline-label.txt', 'a') as file:
-            #         file.write(str(label) + '\n')
-
-            # Gather all the predictions across all the devices to perform ensemble.
-            # if cfg.NUM_GPUS > 1:
-            #     preds, labels, video_idx = du.all_gather(
-            #         [preds, labels, video_idx]
-            #     )
+                
             if cfg.NUM_GPUS:
                 preds = preds.cpu()
                 labels = labels.cpu()
@@ -160,27 +101,6 @@ def perform_test(test_loader, model, test_meter, cfg, writer=None):
             )
             test_meter.log_iter_stats(cur_iter)
 
-            # print('preds:', preds)
-            # print('labels:', labels)
-            """
-            preds: tensor([[1.7187e-01, 4.8815e-01, 3.3998e-01],
-                            [1.6466e-01, 4.9351e-01, 3.4183e-01],
-                            [1.9239e-01, 4.7738e-01, 3.3023e-01],
-                            [2.0546e-02, 5.5177e-01, 4.2768e-01],
-                            [6.8433e-01, 9.5077e-02, 2.2059e-01],
-                            [9.9305e-01, 4.6170e-03, 2.3286e-03],
-                            [7.1990e-01, 2.3570e-01, 4.4403e-02],
-                            [9.9808e-01, 1.2653e-03, 6.5733e-04],
-                            [9.9883e-01, 8.1089e-04, 3.5642e-04],
-                            [9.9875e-01, 8.2948e-04, 4.1667e-04],
-                            [1.0317e-02, 9.6168e-02, 8.9351e-01],
-                            [6.6122e-05, 2.6092e-03, 9.9732e-01],
-                            [1.0126e-04, 3.4601e-04, 9.9955e-01],
-                            [1.0192e-01, 5.6269e-01, 3.3540e-01],
-                            [7.9526e-01, 1.5573e-01, 4.9010e-02],
-                            [1.8888e-02, 5.4777e-01, 4.3334e-01]])
-            labels: tensor([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
-            """
             max_indices = torch.argmax(preds, dim=1)
             # max_indices_array = max_indices.numpy()
             # labels_array = labels.numpy()
@@ -190,18 +110,7 @@ def perform_test(test_loader, model, test_meter, cfg, writer=None):
             labels_array.append(max_indices_id)
             max_indices_array.append(labels_id)
 
-            # precision, recall, f1, _ = precision_recall_fscore_support(labels_array, max_indices_array, average='weighted')
-
-            # precisions.append(precision)
-            # recalls.append(recall)
-            # f1s.append(f1)
-
         test_meter.iter_tic()
-
-    # average_precision = sum(precisions) / len(precisions)
-    # average_recall = sum(recalls) / len(recalls)
-    # average_f1 = sum(f1s) / len(f1s)
-
     precision, recall, f1, _ = precision_recall_fscore_support(labels_array, max_indices_array, average='weighted')
 
     print('precision: ', precision)
@@ -249,7 +158,6 @@ class AttentionSimilarity(nn.Module):
         # 计算注意力得分
         # print('x1.shape:', x1.shape) # (1,2048,8,7,7)
         # (N, C, T, H, W) -> (N, T, H, W, C).
-        print(preds.shape)
         score = torch.matmul(x1.cuda(), preds.transpose(1, 0))
         # 计算注意力权重
         attention_weights_x1 = torch.softmax(score, dim=0)
@@ -297,21 +205,6 @@ def test(cfg):
         assert cfg.NUM_GPUS == cfg.TEST.BATCH_SIZE or cfg.NUM_GPUS == 0
         test_meter = AVAMeter(len(test_loader), cfg, mode="test")
     else:
-        # assert (
-        #     len(test_loader.dataset)
-        #     % (cfg.TEST.NUM_ENSEMBLE_VIEWS * cfg.TEST.NUM_SPATIAL_CROPS)
-        #     == 0
-        # )
-        # # Create meters for multi-view testing.
-        # test_meter = TestMeter(
-        #     len(test_loader.dataset)
-        #     // (cfg.TEST.NUM_ENSEMBLE_VIEWS * cfg.TEST.NUM_SPATIAL_CROPS),
-        #     cfg.TEST.NUM_ENSEMBLE_VIEWS * cfg.TEST.NUM_SPATIAL_CROPS,
-        #     cfg.MODEL.NUM_CLASSES,
-        #     len(test_loader),
-        #     cfg.DATA.MULTI_LABEL,
-        #     cfg.DATA.ENSEMBLE_METHOD,
-        # )
         assert (
                 len(test_loader.dataset)
                 % 1
